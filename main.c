ee69d237095f2be9da45d095ccf41626ea69cdc6
RafaelCF <rafaelcferreira232@gmail.com>
2022-04-14 13:31:10 -0300

    att v3

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "orderSearch.h"
-#define MAX_NAMES 5
+#define MAX_NAMES 3
 
 int main(){
     char names[MAX_NAMES][10], searchName[10];
@@ -14,18 +15,21 @@ int main(){
         printf("5 - Fechar\n");
         printf("Escolha uma das opções acima:\n");
         scanf("%d", &option);
+        system("clear");
         switch (option){
-            case 1: if(total<MAX_NAMES){
+            case 1: if(total<MAX_NAMES){//working
                 if(total==0){
                     printf("Digite o seu nome: ");
                     scanf("%s", names[total]);
+                    system("clear");
                 }else{
                     if(total>0){
                         printf("Digite o seu nome: ");
                         scanf("%s", names[total]);
+                        system("clear");
                         for(int i=0; i<=total-1; i++){
                           if(strcmp(names[total],names[i])==0){
-                            printf("Nome já existente!\n");
+                            printf("Nome já existente!\n\n");
                             total = total -1;
                           }
                         }
@@ -33,10 +37,15 @@ int main(){
                 }
                 total++;
             }else{
-                printf("Lista Cheia\n");
+                printf("Lista Cheia!!! Capacidade da lista é de %d nomes.\n\n", total);
+                for (int i = 0; i < total; i++)
+                {
+                    printf("%s\n", names[i]);
+                }
+                printf("\n");
             }
             break;
-            case 2: insertSort(names, total);
+            case 2: insertSort(names, total);//Not working
             printf("Digite o nome que deseja procurar:");
             scanf("%s", searchName);
             int aux = binarySearch(total, names, searchName);
@@ -47,15 +56,16 @@ int main(){
             }
             //size, names,search
 	        break;
-            case 3: printf("Ordenar lista."); 
-            insertSort(names, total);
+            case 3: printf("Agora sua lista está em ordem alfabética :)\n\n"); 
+            insertSort(names, total);//working
 	        break;
-            case 4: if(total>=0){
-                for(int i=0; i>total; i++){
-                    printf("%s", names[i]);
+            case 4:if(total>0){//Working
+                for(int i=0; i<total; i++){
+                    printf("%s\n", names[i]);
                 }
+                printf("\n");
             }else{
-                printf("Lista vazia");
+                printf("Lista vazia\n\n");
             }
 	        break;
         }
